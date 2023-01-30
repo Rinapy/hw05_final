d1307ed78d961fbcd119386b9b31a94cb38371b2
Roman Minaev <romario123321@yandex.ru>
2023-01-30 11:44:26 +0300

    PEP8

--- yatube/core/views.py
+++ yatube/core/views.py
@@ -3,7 +3,10 @@ from django.shortcuts import render
 
 def page_not_found(request, exception):
     """Страница 404."""
-    return render(request, 'core/errors/404.html', {'path': request.path}, status=404)
+    return render(request,
+                  'core/errors/404.html',
+                  {'path': request.path},
+                  status=404)
 
 
 def csrf_failure(request, reason=''):
