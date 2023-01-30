db211c4eea8e42875285ba1a4f7d2e79f8e65b20
Roman Minaev <romario123321@yandex.ru>
2023-01-30 11:31:07 +0300

    tests follow system end

--- yatube/posts/tests/test_follow.py
+++ yatube/posts/tests/test_follow.py
@@ -1,10 +1,8 @@
 from django.test import TestCase, Client
 from django.contrib.auth import get_user_model
-from django.urls import reverse
-from django.core.files.uploadedfile import SimpleUploadedFile
-from django.core.cache import cache, caches
-from ..models import Post, Group
-from ..forms import PostForm
+from django.core.cache import cache
+from ..models import Post, Follow
+
 
 User = get_user_model()
 
@@ -15,6 +13,7 @@ class TestViewsPostsApp(TestCase):
         super().setUpClass()
         self.user_author = User.objects.create_user(username='TestUser')
         self.user_follower = User.objects.create_user(username='Follower')
+        self.user_not_follower = User.objects.create_user(username='Not_Follower')
         self.post = Post.objects.create(
             text='Test post',
             author=self.user_author,
@@ -24,5 +23,33 @@ class TestViewsPostsApp(TestCase):
 
         self.authorized_client_author = Client()
         self.authorized_user_follower = Client()
+        self.authorized_user_not_follower = Client()
         self.authorized_client_author.force_login(self.user_author)
         self.authorized_user_follower.force_login(self.user_follower)
+        self.authorized_user_not_follower.force_login(self.user_not_follower)
+        cache.clear()
+
+
+    def test_follow_system(self):
+        """Тест подписки на автора"""
+
+        response = self.authorized_user_follower.get(f'/profile/{self.user_author.username}/follow/')
+        self.assertEqual(response.status_code, 302)
+        self.assertTrue(Follow.objects.filter(user=self.user_follower, author=self.user_author))
+
+        response = self.authorized_user_follower.get(f'/profile/{self.user_author.username}/unfollow/')
+        self.assertEqual(response.status_code, 302)
+        self.assertFalse(Follow.objects.filter(user=self.user_follower, author=self.user_author))
+
+    def test_follow_page(self):
+        """Подписки отображаються верно"""
+
+        self.authorized_user_follower.get(f'/profile/{self.user_author.username}/follow/')
+        response = self.authorized_user_follower.get(f'/follow/')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.context['page_obj']), 1)
+
+        response = self.authorized_user_not_follower.get(f'/follow/')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.context['page_obj']), 0)
+        
